78bb435e213c50e5d156ee93b8c3c0e2c4ac80e8
slucaslima <lucas.emanuellima@hotmail.com>
2022-03-02 21:49:06 -0300

    exercicio3

---
+++ Exercicio3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a distância percorrida em KM:");
+            string distanciaPercorrida = Console.ReadLine();
+
+            Console.WriteLine("Informe a quantidade de combústivel utilizada em Litros:");
+            string combustivelUtilizado = Console.ReadLine();
+
+            decimal distanciaConvertida = 0.0m;
+            decimal combustivelConvertido = 0.0m;
+
+            decimal.TryParse(distanciaPercorrida, out distanciaConvertida);
+            decimal.TryParse(combustivelUtilizado, out combustivelConvertido);
+
+            decimal consumoFinal = distanciaConvertida / combustivelConvertido;
+
+            Console.WriteLine($"O consumo do veículo foi de: {consumoFinal} Km/L");
+            Console.ReadLine();
+        }
+    }
+}
