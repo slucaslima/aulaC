b5f16855a2851f09dfc29d0dc3d00bbb9cddf32b
slucaslima <lucas.emanuellima@hotmail.com>
2022-03-02 21:14:45 -0300

    exercicio2

---
+++ Exercicio2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o valor em $: ");
+
+            string valorDigitado = Console.ReadLine();
+            decimal valorConvertido = 0.0m;
+            decimal valorCambio = 5.22m;
+
+            decimal.TryParse(valorDigitado, out valorConvertido);
+
+            decimal valorCambioConvertido = valorConvertido * valorCambio;
+
+            Console.WriteLine($"Valor em R$: {valorCambioConvertido}");
+            Console.ReadLine();
+
+
+        }
+    }
+}
