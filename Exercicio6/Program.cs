a11ced66bda07630f1369aa4686f1fd34ae8d8bf
slucaslima <lucas.emanuellima@hotmail.com>
2022-03-09 22:32:17 -0300

    Exercícios do dia 09/03/22

---
+++ Exercicio6/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Exercicio6
+{
+    class Program
+    {
+        private const char _a = 'a';
+        private const char _e = 'e';
+        private const char _i = 'i';
+        private const char _o = 'o';
+        private const char _u = 'u';
+
+        static void Main(string[] args)
+        {
+            int count = 0;
+
+            try
+            {
+                Console.WriteLine("Informe o seu nome:");
+                string nome = Console.ReadLine().ToLower();
+
+                for (int i = 0; i <= nome.Length; i++)
+                {
+                    if (_a.Equals(nome[i])
+                        || _e.Equals(nome[i])
+                        || _i.Equals(nome[i])
+                        || _o.Equals(nome[i])
+                        || _u.Equals(nome[i]))
+                        count++;
+                }
+
+                Console.WriteLine($"Seu nome possui {count} vogal(is)!");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
