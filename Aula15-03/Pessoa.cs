cbcdf0b94936af4925d0a9f12f61b24c59f57b22
Humberto Petita <humbertopetita@gmail.com>
2022-03-15 22:25:43 -0300

    Aula 15-03

---
+++ Aula15-03/Pessoa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula15_03
+{
+    class Pessoa
+    {
+        public string Nome { get; private set; }
+
+        public Pessoa (string nome)
+        {
+            Nome = nome ??
+                throw new ArgumentNullException(nameof(nome));
+        }
+    }
+}
---
+++ Aula15-03/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Aula15_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome1 = "Lucas";
+            string nome2 = nome1;
+
+            nome1 = "Dog";
+
+            Console.WriteLine($"{nameof(nome1)}:{nome1}");
+            Console.WriteLine($"{nameof(nome2)}:{nome2}");
+
+            Console.ReadLine();
+        }
+    }
+}
