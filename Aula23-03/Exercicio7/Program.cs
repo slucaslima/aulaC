76511b01dbce885e18552ef4c53464a4657df792
slucaslima <lucas.emanuellima@hotmail.com>
2022-03-23 20:32:53 -0300

    Aula 23-03

---
+++ Aula23-03/Exercicio7/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Exercicio7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                Console.WriteLine("Informe seu nome:");
+                var name = Console.ReadLine();
+
+                Console.WriteLine("Informe sua data de nascimento em dd/mm/YY:");
+                var date = DateTime.Parse(Console.ReadLine());
+
+                Console.WriteLine(ObterMensagemFaixaEtaria(name, date));
+
+            } 
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            } 
+            finally
+            {
+                Console.ReadLine();
+            }
+
+
+        }
+
+        private static string ObterMensagemFaixaEtaria(string name, DateTime date)
+        {
+            var idade = DateTime.Today.Year - date.Year;
+
+            if (idade >= 60)
+                return $"Olá {name}, você possui {idade} anos e está na faixa etária de Idosos!";
+            else if (idade >= 20 && idade <= 59)
+                return $"Olá {name}, você possui {idade} anos e está na faixa etária de Adultos!";
+
+            return $"Olá {name}, você possui {idade} anos e está na faixa etária de Jovens!";
+        }
+    }
+}
---
+++ Aula23-03/Exercicio8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercicio8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+
+
+
+
+
+
+        }
+    }
+}
